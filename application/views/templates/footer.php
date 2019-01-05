9b980cc70af3547e35caa6987e748ff24074528e
Yuriko Aya <thechildoflily@gmail.com>
2019-01-05 18:59:01 +0700

    First commit

---
+++ application/views/templates/footer.php
@@ -0,0 +1,14 @@
+<!-- jQuery 3 -->
+<script src="assets/js/jquery.slim.min.js"></script>
+<!-- jQuery UI 1.11.4 -->
+<script src="assets/js/jquery-ui.min.js"></script>
+<!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
+<script>
+  $.widget.bridge('uibutton', $.ui.button);
+</script>
+<!-- Bootstrap 3.3.7 -->
+<script src="assets/js/bootstrap.min.js"></script>
+<!-- AdminLTE App -->
+<script src="assets/js/adminlte.min.js"></script>
+</body>
+</html>
